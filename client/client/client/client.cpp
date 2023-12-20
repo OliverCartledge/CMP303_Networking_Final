bc9e7a0c77023fa7afd15c95e71f80638be260d0
Oliver Cartledge <olivercartledge@yahoo.com>
2023-12-20 14:25:42 +0000

    temp push for going home

--- client/client/client/client.cpp
+++ client/client/client/client.cpp
@@ -1,42 +1,183 @@
-#include <SFML/Network/UdpSocket.hpp>
-#include <iostream>
-using std::to_string;
+//#include <SFML/Network/UdpSocket.hpp>
+//#include <iostream>
+//using std::to_string;
+//
+//// The (fixed) size of message that we send between the two programs
+//#define MESSAGESIZE 40
+//
+//int main()
+//{
+//	// Create a socket and bind it to the port 55001
+//	sf::UdpSocket socket;
+//	socket.bind(55001);
+//
+//	int test = 0;
+//
+//	char buffer2[MESSAGESIZE];
+//
+//	do
+//	{
+//		//Testing adding to one and the other at the same time
+//		//Test push
+//		// Send a message to 192.168.1.50 on port 55002
+//		std::string message = "Hi, I am " + sf::IpAddress::getLocalAddress().toString();
+//		socket.send(message.c_str(), message.size() + 1, sf::IpAddress::getLocalAddress().toString(), 55002);
+//
+//		// Receive an answer (most likely from 192.168.1.50, but could be anyone else)
+//		char buffer[1024];
+//		std::size_t received = 0;
+//		sf::IpAddress sender;
+//		unsigned short port;
+//		socket.receive(buffer, sizeof(buffer), received, sender, port);
+//		std::cout << sender.toString() << " said: " << buffer << std::endl;
+//
+//		std::cin >> buffer2;
+//		socket.send(buffer2, MESSAGESIZE, sf::IpAddress::getLocalAddress().toString(), 55002);
+//	} while (memcmp(buffer2, "quit", 4) != 0);
+//
+//
+//
+//	return 0;
+//}
+//
 
-// The (fixed) size of message that we send between the two programs
-#define MESSAGESIZE 40
+#include <iostream>
+#include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <chrono>
+#include <box2d/box2d.h>
+
+
+#include<thread>
+#include<vector>
+
+int width = 1024;
+int height = 768;
+using namespace std;
+
+int playerID;
+
+//Networking varibales 
+sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+//sf::IpAddress ip;
+sf::TcpSocket client;
+sf::Packet packetSend, packetRecieved;
+std::thread recieveThread;
+bool recieved = false;
+
+
+sf::RenderWindow* window;
+
+
+
+
+void sendtoServer()
+{
+	client.send(packetSend);
+	packetSend.clear();
+}
+void recievefromServer()
+{
+	packetRecieved.clear();
+	if (client.receive(packetRecieved) == sf::Socket::Done) {
+		recieved = true;
+	}
+	else
+		recieved = false;
+}
+
+// Begins rendering to the back buffer. Background colour set to light blue.
+void beginDraw()
+{
+	window->clear(sf::Color(0, 0, 0));
+}
+
+// Ends rendering to the back buffer, and swaps buffer to the screen.
+void endDraw()
+{
+	window->display();
+}
+
+
+void render()
+{
+	beginDraw();
+
+	//map texture 
+	sf::Texture mapText;
+	sf::Sprite map;
+
+	//loading map
+	mapText.loadFromFile("media/controlsScreen.png");
+	mapText.setSmooth(true);
+	map.setTexture(mapText);
+	map.scale(2, 2);
+
+	window->draw(map);
+
+	endDraw();
+}
+
 
 int main()
 {
-	// Create a socket and bind it to the port 55001
-	sf::UdpSocket socket;
-	socket.bind(55001);
+	sf::RenderWindow window(sf::VideoMode(960, 540), "CMP303");
+	window.setFramerateLimit(60);
 
-	int test = 0;
+	
 
-	char buffer2[MESSAGESIZE];
+	// Initialise objects for delta time
+	sf::Clock clock;
+	float deltaTime;
+	int frame_rate;
 
-	do
+	//connecting to server and getting playerId 
+	std::string name;
+	std::string IP;
+	cout << "Enter IP: ";
+	std::cin >> IP;
+	ip = IP;
+	cout << "\nConnected to " << ip.toString() << "\n";
+	while (client.connect(ip, 2000) != sf::Socket::Done) {
+		cout << "Failed to connect, trying again...\n";
+	}
+	cout << "connected to server\n";
+	cout << "Enter Player Name: ";
+	std::cin >> name;
+	packetSend << name;
+	sendtoServer();
+	recievefromServer();
+	packetRecieved >> playerID;
+
+	cout << "\nyou are player: " << playerID << endl;
+	playerID = playerID - 1;
+
+	client.setBlocking(false);
+
+	while (window.isOpen())
 	{
-		//Testing adding to one and the other at the same time
-		//Test push
-		// Send a message to 192.168.1.50 on port 55002
-		std::string message = "Hi, I am " + sf::IpAddress::getLocalAddress().toString();
-		socket.send(message.c_str(), message.size() + 1, sf::IpAddress::getLocalAddress().toString(), 55002);
+		deltaTime = clock.restart().asSeconds();
+		sf::Event event;
 
-		// Receive an answer (most likely from 192.168.1.50, but could be anyone else)
-		char buffer[1024];
-		std::size_t received = 0;
-		sf::IpAddress sender;
-		unsigned short port;
-		socket.receive(buffer, sizeof(buffer), received, sender, port);
-		std::cout << sender.toString() << " said: " << buffer << std::endl;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::KeyPressed) 
+			{
+				if (event.key.code == sf::Keyboard::Key::Escape)
+					window.close();
+				if (event.key.code == sf::Keyboard::Key::R) {
+					/*tanks[0].Reset(); tanks[1].Reset();
+					netSimulator.Reset();*/
+					//nextPrint = startTime;
+					printf("\n\n--------RESET--------\n\n");
+				}
+			}
+		}
+	}
 
-		std::cin >> buffer2;
-		socket.send(buffer2, MESSAGESIZE, sf::IpAddress::getLocalAddress().toString(), 55002);
-	} while (memcmp(buffer2, "quit", 4) != 0);
-
-
+	render();
 
 	return 0;
 }
-
--- server/server/server/server.cpp
+++ server/server/server/server.cpp
@@ -1,31 +1,119 @@
+//#include <SFML/Network.hpp>
+//#include <iostream>
+//using std::to_string;
+//
+//int main()
+//{
+//	// Create a socket and bind it to the port 55002
+//	sf::UdpSocket socket;
+//	socket.bind(55002);
+//
+//	//std::cout << "Hi, I am " + sf::IpAddress::getLocalAddress().toString();
+//	std::cout << "Waiting on client joining\n";
+//	while (true)
+//	{
+//
+//		//Test push
+//		// //Test again again
+//		// Receive a message from anyone
+//		char buffer[1024];
+//		std::size_t received = 0;
+//		sf::IpAddress sender;
+//		unsigned short port;
+//		socket.receive(buffer, sizeof(buffer), received, sender, port);
+//		std::cout << sender.toString() << " said: " << buffer << std::endl;
+//
+//		// Send an answer
+//		std::string message = "Welcome " + sender.toString();
+//		socket.send(message.c_str(), message.size() + 1, sender, port);
+//	}
+//
+//	return 0;
+//}
+
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
-using std::to_string;
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-	// Create a socket and bind it to the port 55002
-	sf::UdpSocket socket;
-	socket.bind(55002);
+	cout << "Server is running\n";
 
-	//std::cout << "Hi, I am " + sf::IpAddress::getLocalAddress().toString();
-	std::cout << "Waiting on client joining\n";
-	while (true)
+	sf::TcpListener listener;
+	sf::SocketSelector selector;
+	std::vector<sf::TcpSocket*> clients;
+	std::vector<std::string> ID;
+
+	bool test = false;
+
+	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+	cout << "The servers IP address is " << ip << endl;
+	listener.listen(2000);
+	selector.add(listener);
+
+	while (!test)
 	{
+		if (selector.wait())
+		{
+			if (selector.isReady(listener))
+			{
+				sf::TcpSocket* socket = new sf::TcpSocket;
+				std::string id;
+				listener.accept(*socket);
+				sf::Packet packet;
+				if (socket->receive(packet) == sf::Socket::Done)
+					packet >> id;
+				cout << id << " has connected to the server" << endl;
+				clients.push_back(socket);
+				ID.push_back(id);
+				selector.add(*socket);
+				packet.clear();
+				int playerId;
+				playerId = clients.size();
+				packet << playerId;
+				socket->send(packet);
+				if (clients.size() > 4)
+				{
+					cout << "Too many people";
+					listener.close();
+				}
+			}
+		}
+		else
+			for (int i = 0; i < clients.size(); i++)
+			{
+				if(selector.isReady(*clients[i]))
+				{
+					sf::Packet packet, sendPacket;
 
-		//Test push
-		// //Test again again
-		// Receive a message from anyone
-		char buffer[1024];
-		std::size_t received = 0;
-		sf::IpAddress sender;
-		unsigned short port;
-		socket.receive(buffer, sizeof(buffer), received, sender, port);
-		std::cout << sender.toString() << " said: " << buffer << std::endl;
+					if (clients[i]->receive(packet) == sf::Socket::Done)
+					{
+						for (int j = 0; j < clients.size(); j++)
+						{
+							if (i != j)
+							{
+								clients[j]->send(packet);
+							}
+						}
+					}
+					if (clients[i]->receive(packet) == sf::Socket::Disconnected)
+					{
+						cout << ID[i] << " disconnected from the server\n";
+						selector.remove(*clients[i]);
+						clients.erase(find(clients.begin(), clients.end(), clients[i]));
+						ID.erase(find(ID.begin(), ID.end(), ID[i]));
+					}
+				}
+			}
+	}
 
-		// Send an answer
-		std::string message = "Welcome " + sender.toString();
-		socket.send(message.c_str(), message.size() + 1, sender, port);
+	for (std::vector<sf::TcpSocket*>::iterator it = clients.begin(); it != clients.end(); it++)
+	{
+		delete* it;
 	}
 
 	return 0;
